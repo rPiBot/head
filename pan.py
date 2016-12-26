6d735b4ed223ce397d5a320128c7e32b4c0fe1f3
rPiBot <rPiBot>
2016-12-26 16:45:19 +0000

    XboxPan

--- pan.py
+++ pan.py
@@ -3,10 +3,15 @@ import time
 
 ss = ServoSix()
 
-smoothness = 0.05
+speed = 0.015
 
-for x in range(0, 90):
-  ss.set_servo(1, x)
-  time.sleep(smoothness*10)
+stop_moving = False
+
+for x in range(0, 180):
+    if stop_moving:
+        stop_moving = False
+        break
+    ss.set_servo(1, x)
+    time.sleep(speed)
 
 ss.cleanup()
