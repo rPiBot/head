a84295b79aa55c16be07cbf42351717a5eae0381
rPiBot <rPiBot>
2016-12-28 21:11:41 +0000

    Controls

--- controls.py
+++ controls.py
@@ -3,30 +3,17 @@ from pygame.locals import *
 from servosix import ServoSix
 
 pygame.init()
+screen = pygame.display.set_mode((1, 1)) #TODO Required?
+pygame.key.set_repeat(100, 50)
+
 ss = ServoSix()
 
-screen = pygame.display.set_mode((1, 1)) #TODO Required?
-pygame.key.set_repeat(100, 20)
-
-
-steps = { 'size': 10, 'delay': 0.05, 'range_min': 20, 'range_max': 160 }
+steps = { 'size': 10, 'range_min': 20, 'range_max': 160 }
 cam = {'x': 90, 'y': 90}
-allow = { 'x': True, 'y': True }
-
-class start_pan_tilt(threading.Thread):
-    def __init__(self, type, direction):
-        threading.Thread.__init__(self)
-        self.type = type
-        self.direction = direction
-    def run(self):
-        print 'strting thread-------------------'
-        pan_tilt(self.type, self.direction)
-
 
 def pan_tilt(type, direction):
-    global cam, steps, allow
+    global cam, steps
 
-#    while allow[type]:
     if (cam[type] <= steps['range_min'] and direction == 'negative') or (cam[type] >= steps['range_max'] and direction == 'positive'):
         print 'Limit reached'
 
@@ -35,20 +22,12 @@ def pan_tilt(type, direction):
             cam[type] = cam[type] + steps['size']
         else:
             cam[type] = cam[type] - steps['size']
-            print type, direction, cam[type]
 
         servo = 1 if type == 'x' else 2
         ss.set_servo(servo, cam[type])
 #        percent = (cam[type] / 180) * 100
 #        os.system("echo {}={}% > /dev/servoblaster".format(servo, percent))
-#    time.sleep(steps['delay'])
-    print 'END -----------------'
     return True
-
-def stop(type):
-    global allow
-    allow[type] = False
-    print type, 'released'
 
 while True:
     for event in pygame.event.get():
@@ -61,26 +40,13 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                #thread.start_new_thread(pan_tilt, ('x', 'positive',))
-                #s = start_pan_tilt('x', 'positive')
                 pan_tilt('x', 'positive')
             elif event.key == pygame.K_RIGHT:
-                #`thread.start_new_thread(pan_tilt, ('x', 'negative',))
-                #s = start_pan_tilt('x', 'negative')
                 pan_tilt('x', 'negative')
             elif event.key == pygame.K_UP:
-                #thread.start_new_thread(pan_tilt, ('y', 'negative',))
                 pan_tilt('y', 'negative')
             elif event.key == pygame.K_DOWN:
-                #thread.start_new_thread(pan_tilt, ('y', 'positive',))
                 pan_tilt('y', 'positive')
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                stop('x')
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                stop('y')
-        #s.start
 
 ss.cleanup()
 pygame.quit()
