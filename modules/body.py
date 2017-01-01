69289e9bf6d1fb3ce796641a5f9ba77565d553b1
rPiBot <rPiBot>
2017-01-01 15:36:54 +0000

    v1

--- modules/body.py
+++ modules/body.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(35, GPIO.OUT)
@@ -6,9 +7,29 @@ GPIO.setup(36, GPIO.OUT)
 GPIO.setup(37, GPIO.OUT)
 GPIO.setup(38, GPIO.OUT)
 
+TRIG = 33
+ECHO = 31
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
 class Body:
     state = ''
 
+    def check_distance(self):
+        sleep(0.005) # Wait for sensor to be ready
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO) == 0:
+          start = time.time()
+
+        while GPIO.input(ECHO) == 1:
+          end = time.time()
+
+        duration = end - start
+        distance = round((duration * 17150), 2)
+        print "Distance:",distance,"cm"
 
     def move(self, direction):
         if direction != self.state:
@@ -17,9 +38,12 @@ class Body:
             GPIO.output(37, False)
             GPIO.output(38, False)
 
+            GPIO.output(TRIG, False)
+
             self.state = direction
 
             if direction == 'forwards':
+                check_distance(self)
                 GPIO.output(35, True)
                 GPIO.output(36, True)
             elif direction == 'backwards':
